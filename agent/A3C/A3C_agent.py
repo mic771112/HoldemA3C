a467d7d822d88fc7c22017e11fcfb3c6162a6c9e
mic771112 <r00222020@ntu.edu.tw>
2018-08-09 21:11:06 +0800

    Update A3C_agent.py

--- agent/A3C/A3C_agent.py
+++ agent/A3C/A3C_agent.py
@@ -292,9 +292,9 @@ if __name__ == '__main__':
     uris = ['ws://poker-battle.vtr.trendnet.org:3001'] + \
            ['ws://poker-training.vtr.trendnet.org:3001/'] * len(range(1, 19)) * 2
 
-    names = ['1886368b064b4b76be10d54d38958ce3'] +\
-            ['omg{}'.format(str(i)) for i in range(1, 19)] + \
-            ['omgt{}'.format(str(i)) for i in range(1, 19)]
+    names = ['x'] +\
+            ['xx{}'.format(str(i)) for i in range(1, 19)] + \
+            ['xxx{}'.format(str(i)) for i in range(1, 19)]
 
     assert len(uris) == len(names)
     agent = A3CAgent(model_dir=model_dir,
